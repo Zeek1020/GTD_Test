2a907869e8ec12f370a50afc72e4e838a9389a33
Zeek1020 <nmartin1893@gmail.com>
2018-02-11 11:03:31 -0500

    TabLayout listener is deprecated. What now?

--- app/src/main/java/com/example/nick/gettingthingsdone/Tab_Activity.java
+++ app/src/main/java/com/example/nick/gettingthingsdone/Tab_Activity.java
@@ -4,6 +4,8 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -20,66 +22,43 @@ import java.util.List;
 
 public class Tab_Activity extends AppCompatActivity {
 
-    TabHost host;
-
-    protected void addTab( TabHost t, String tabName, int viewID ){
-        TabHost.TabSpec ts = t.newTabSpec( tabName );
-        ts.setContent( viewID );
-        ts.setIndicator( tabName );
-        t.addTab( ts );
-    }
-
-    TabHost.OnTabChangeListener tabListener = new TabHost.OnTabChangeListener(){
+    TabLayout.OnTabSelectedListener changeTab = new TabLayout.OnTabSelectedListener(){
         @Override
-        public void onTabChanged( String name ){
-            Toast.makeText( Tab_Activity.this, name, Toast.LENGTH_SHORT );
+        public void onTabUnselected(TabLayout.Tab tab) {
+
+        }
+
+        @Override
+        public void onTabReselected(TabLayout.Tab tab) {
+
+        }
+
+        @Override
+        public void onTabSelected(TabLayout.Tab tab) {
+
         }
     };
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final ViewPager vPager = ( ViewPager ) findViewById( R.id.pager );
+        TabLayout tLayout = ( TabLayout ) findViewById( R.id.tab_layout );
+        final PagerAdapter pagerAdapter = new PagerAdapter( getSupportFragmentManager(), tLayout.getTabCount());
+        vPager.setAdapter( pagerAdapter );
 
-        host = findViewById( R.id.TH_Inboxes );
-
-        host.setup();
-        addTab(host, "Next Action", R.id.Next_Action);
-        addTab(host, "Projects", R.id.Projects);
-        addTab(host, "Reference", R.id.Reference);
-
-        host.setOnTabChangedListener( tabListener );
-        String tag = "DEBUG";
-
-        ListView v = null;
-
-        View tab_view = host.getCurrentTabView();
+        tLayout.addTab( tLayout.newTab().setText("Tab1") );
+        tLayout.addTab( tLayout.newTab().setText("Tab2") );
+        tLayout.addTab( tLayout.newTab().setText("Tab3") );
+        tLayout.setTabGravity( TabLayout.GRAVITY_FILL );
+        //TODO: Why is this deprecated?  What do I use instead?
+        tLayout.setOnTabSelectedListener( changeTab );
 
         //TODO: http://www.truiton.com/2015/06/android-tabs-example-fragments-viewpager/ fragment pager?
 
-        try{
-            v = ( ListView ) host.getCurrentTabView().findViewById( R.id.LV_NEXT_ACTION );
-        } catch ( Exception e ){
-            Log.d( tag, e.toString() );
-            Log.d( tag, host.getCurrentTabView().toString() );
-        }
 
-        ArrayList<String> al = new ArrayList<>();
-        al.add( "Do Something!" );
-        al.add( "File Taxes" );
-        al.add( "What the hell is all this then?");
-        ArrayAdapter<String> ad = new ArrayAdapter<String>( Tab_Activity.this, android.R.layout.simple_list_item_1, al);
-        try {
-            v.setAdapter(ad);
-        } catch ( Exception e ){
-            String s = e.getMessage();
-            if ( s == null ){
-                s = e.toString();
-            }
-            Log.d( tag, s);
-        }
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
